61cae9fea858dd12169ef8bcba19ed37e506598a
regenerik <regenerik@gmail.com>
2024-07-30 19:07:52 -0300

    fix mapeado de acentos

--- admin_bp.py
+++ admin_bp.py
@@ -154,8 +154,22 @@ def descargar_reporte():
         'Ã': 'A', 'Õ': 'O', 'Ä': 'A', 'Ë': 'E', 'Ï': 'I',
         'Ö': 'O', 'Ü': 'U', 'Ç': 'C'
     }
-
+    def cargar_accent_mapping():
+        return {
+        'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
+        'à': 'a', 'è': 'e', 'ì': 'i', 'ò': 'o', 'ù': 'u',
+        'â': 'a', 'ê': 'e', 'î': 'i', 'ô': 'o', 'û': 'u',
+        'ã': 'a', 'õ': 'o', 'ä': 'a', 'ë': 'e', 'ï': 'i',
+        'ö': 'o', 'ü': 'u', 'ç': 'c',
+        'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U',
+        'À': 'A', 'È': 'E', 'Ì': 'I', 'Ò': 'O', 'Ù': 'U',
+        'Â': 'A', 'Ê': 'E', 'Î': 'I', 'Ô': 'O', 'Û': 'U',
+        'Ã': 'A', 'Õ': 'O', 'Ä': 'A', 'Ë': 'E', 'Ï': 'I',
+        'Ö': 'O', 'Ü': 'U', 'Ç': 'C'
+        }
     # Reemplazar acentos
+    if accent_mapping is None:
+        accent_mapping = cargar_accent_mapping()
     title = ''.join(accent_mapping.get(c, c) for c in title)
 
     # Reemplazar caracteres no válidos en nombres de archivos
