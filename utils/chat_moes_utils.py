cc89df90e131d23f54ab304d4fb5f2ea47addf9c
regenerik <regenerik@gmail.com>
2025-04-23 20:56:20 -0300

    prefix on system

--- utils/chat_moes_utils.py
+++ utils/chat_moes_utils.py
@@ -28,17 +28,16 @@ def query_assistant(prompt: str, thread_id: Optional[str] = None) -> Tuple[str, 
     """
 
     # Texto de introducción fijo
-    instruction_prefix = (
-        "Se te va a presentar una pregunta relacionada con MOES o YPF. "
-        "Tu tarea es responder exclusivamente sobre esos temas. Si el usuario realiza una consulta "
-        "que no esté vinculada con MOES o YPF, respondé que solo estás habilitado para asistir en temas relacionados a MOES de YPF.\n\n"
-        "Además, si el usuario pregunta quién sos, cómo funcionás o si sos un experto, aclarales que sos un asistente creado por YPF "
-        "para asistir en consultas vinculadas al contenido del MOES.\n\n"
-        "Es importante que formatees tus respuestas con saltos de línea donde sea necesario para facilitar la lectura.\n\n"
-        "A continuación, la consulta del usuario:\n\n"
-    )
+    # instruction_prefix = (
+    #     "Se te va a presentar una pregunta relacionada con MOES o YPF. "
+    #     "Tu tarea es responder exclusivamente utilizando el contenido relacionado con esos temas a los cuales ya tienes acceso.\n\n"
+    #     "Además, si el usuario pregunta quién sos, cómo funcionás o si sos un experto, aclarales que sos un asistente creado por YPF "
+    #     "para asistir en consultas vinculadas al contenido del MOES.\n\n"
+    #     "Es importante que formatees tus respuestas con saltos de línea donde sea necesario para facilitar la lectura.\n\n"
+    #     "A continuación, la consulta del usuario:\n\n"
+    # )
 
-    full_prompt = instruction_prefix + prompt
+    full_prompt =  prompt
 
     if thread_id:
         # Continuar hilo existente
